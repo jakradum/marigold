2336d4ceb116601a082d69ddf057c837853c2558
jakradum <145890080+jakradum@users.noreply.github.com>
2023-11-13 18:03:45 +0530

    animated the header

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let colorLogo = document.querySelectorAll(`.logo`);
 function toggleX(){bars.forEach(i=>i.classList.toggle(`x`))}
 let navBar = document.querySelectorAll(`.mobile-menu`);
 let header = document.querySelectorAll(`.headersec`);
+let menuItems = document.querySelectorAll(`.mobile-dropdown`);
 
 navToggle.addEventListener(`click`,toggleX);
 navToggle.addEventListener(`click`,function(){colorLogo.forEach(i=>i.classList.toggle(`bw`));})
 navToggle.addEventListener(`click`,function(){navBar.forEach(i=>i.classList.toggle(`drop`));})
 navToggle.addEventListener(`click`,function(){header.forEach(i=>i.classList.toggle(`drop`));})
+navToggle.addEventListener(`click`,function(){menuItems.forEach(i=>i.classList.toggle(`show`));})
